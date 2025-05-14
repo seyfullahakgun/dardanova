ca2501ec6ca7c39a49e42f3e6e37bbaed69c2419
Seyfullah Akgün <seyfullah.akgun@coens.io>
2025-05-14 04:54:21 +0300

    bugifx

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,5 @@
 import { getRequestConfig } from 'next-intl/server';
-import { locales, defaultLocale } from './i18n/routing';
+import { defaultLocale } from './i18n/routing';
 
 export default getRequestConfig(async ({ locale }) => {
   const currentLocale = locale || defaultLocale;
